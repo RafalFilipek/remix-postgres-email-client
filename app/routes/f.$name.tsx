ebf27ef1e13c97aaa0d8a104e612f4535d053324
Rafał Filipek <rafal.filipek@gmail.com>
2023-11-07 09:41:44 +0100

    fix: proper empty view logic

--- app/routes/f.$name.tsx
+++ app/routes/f.$name.tsx
@@ -1,11 +1,6 @@
-import {
-  Outlet,
-  useLoaderData,
-  useLocation,
-  useParams,
-} from "@remix-run/react";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import { EmailListColumn } from "../components/email-list-column";
-import { EmailEmptyView } from "../components/empty-email-view";
+
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { getEmailsForFolder } from "../db/queries";
@@ -22,20 +17,12 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
 export default function Component() {
   const { emails, folderName } = useLoaderData<typeof loader>();
-  const { id } = useParams();
-  const { pathname } = useLocation();
-
-  // Yo, help me here!
-  // https://twitter.com/rafalfilipek/status/1721626127901319490
-  const showEmptyView = !pathname.endsWith("/new") && !id;
-
-  console.log({ showEmptyView });
 
   return (
     <>
       <EmailListColumn folderName={folderName} emails={emails} />
-      {showEmptyView && <EmailEmptyView />}
-      {!showEmptyView && <Outlet />}
+
+      <Outlet />
     </>
   );
 }
